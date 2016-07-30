7219c18c8bfa943b1d3cafd95be0190d7b4c32e2
mdodsworth <michael.dodsworth@optimizely.com>
2016-07-29 17:24:46 -0700

    WIP

--- triangle.py
+++ triangle.py
@@ -1,13 +1,24 @@
 from pyglet.gl import *
 
-BOID_SIZE = 5
+BOID_WIDTH = 20
+BOID_LENGTH = int(BOID_WIDTH * 1.2)
 
 window = pyglet.window.Window()
 
+# move this to somewhere useful
+template = gl.Config(sample_buffers=1, samples=4)
+try:
+        config = screen.get_best_config(template)
+except pyglet.window.NoSuchConfigException:
+    template = gl.Config()
+    config = screen.get_best_config(template)
+
+window = pyglet.window.Window(config=config)
+
 vertices = [
         0, 0,
-        window.width, 0,
-        window.width, window.height]
+        BOID_WIDTH, 0,
+        BOID_WIDTH // 2, BOID_LENGTH]
 vertices_gl = (GLfloat * len(vertices))(*vertices)
 
 glEnableClientState(GL_VERTEX_ARRAY)
